9240f793fad12a6cdf2110471b54914691f1cb12
David E. Wheeler <david@kineticode.com>
2003-09-04 05:31:14 +0000

    Fixed each, plus a number of other things. Much more like Apache::Table, now

--- lib/Apache/FakeTable.pm
+++ lib/Apache/FakeTable.pm
@@ -11,56 +11,83 @@ Apache::FakeTable - Pure Perl implementation of the Apache::Table interface.
 
   use Apache::FakeTable;
 
-  my $table = Apache::FakeTable->new(@vals);
+  my $table = Apache::FakeTable->new($r);
+
   $table->set(From => 'david@kineticode.com');
 
   $table->add(Cookie => 'One Cookie');
   $table->add(Cookie => 'Another Cookie');
 
-  $table->do( sub {
-      my ($k, $v) = @_;
-      print "$k: $v\n";
-  });
+  while(my($key, $val) = each %$table) {
+      print "$key: $val\n";
+  }
 
 =head1 DESCRIPTION
 
-This class emulates the behavior of the L<Apache::Table|Apache::Table> class.
+This class emulates the behavior of the L<Apache::Table|Apache::Table> class,
+and is designed to behave exactly like Apache::Table. This means that all keys
+are case-insensitive and may have multiple values. As a drop-in substitute for
+Apache::Table, you should be able to use it exactly like Apache::Table.
 
-Apache::FakeTable is designed to behave exactly like Apache::Table, and
-differs in only one respect. When a given key has multiple values in an
-C<Apache::Table> object, one can fetch each of the values for that key using
-Perl's C<each> operator:
+You can treat an Apache::FakeTable object much like any other hash. However,
+like Apache Table, those keys that contain multiple values will trigger
+slightly different behavior than a traditional hash. The variations in
+behavior are as follows:
 
-  while (my ($k, $v) = each %$table) {
-      push @cookies, $v if lc $k eq 'set-cookie';
-  }
+=over
 
-If anyone knows how Apache::Table does this, let us know! In the meantime, use
-C<get()> or C<do()> to get at all of the values for a given key (they're much
-more efficient, anyway).
+=item keys
+
+Will return the same key multiple times, once for each value stored for that
+key.
+
+=item values
+
+Will return the first value multiple times, once for each value stored for a
+given key. It'd be nice if it returned all the values for a given key, instead
+of the first value C<*> the number of values, but that's not the way
+Apache::Table works, and I'm not sure I'd know how to implement it even if it
+did!
+
+=item each
+
+Will return the same key multiple times, pairing it with each of its values
+in turn.
+
+=back
+
+Otherwise, things should be quite hash-like, particularly when a key has only
+a single value.
 
 =head1 INTERFACE
 
 =head3 new()
 
-  my $table = Apache::FakeTable->new(@values);
+  my $table = Apache::FakeTable->new($r);
+  $table = Apache::FakeTable->new($r, $initial_size);
 
-Returns a new C<Apache::FakeTable> object. Any parameters passed
-to C<new()> will be added to the table as initial values using C<set()>.
+Returns a new C<Apache::FakeTable> object. An L<Apache|Apache> object is
+required as the first argument. An optional second argument sets the initial
+size of the table for storing values.
 
 =cut
 
 sub new {
-    my $class = shift;
+    # We actually ignore the optional argument.
+    my ($class, $r) = @_;
+    unless (UNIVERSAL::isa($r, 'Apache')) {
+        require Carp;
+        Carp::croak "Usage: " . __PACKAGE__ . "::new(pclass, r, nalloc=10)";
+    }
     my $self = {};
     tie %{$self}, 'Apache::FakeTableHash';
-    %$self = @_ if @_;
     return bless $self, ref $class || $class;
 }
 
 =head3 get()
 
   my $value = $table->get($key);
+  my @values = $table->get($key);
   my $value = $table->{$key};
 
 Gets the value stored for a given key in the table. If a key has multiple
@@ -80,14 +107,20 @@ sub get {
 
 Takes key and value arguments and sets the value for that key. Previous values
 for that key will be discarded. The value must be a string, or C<set()> will
-turn it into one. A value of C<undef> will have the same behavior as
-C<unset()>.
+turn it into one. A value of C<undef> will be converted to the null string
+('') a warning will be issued if warnings are enabled.
 
 =cut
 
 sub set {
     my ($self, $header, $value) = @_;
-    defined $value ? $self->{$header} = $value : delete $self->{$header};
+    # Issue a warning if the value is undefined.
+    if (! defined $value and $^W) {
+        require Carp;
+        Carp::carp "Use of uninitialized value in null operation";
+        $value = '';
+    }
+    $self->{$header} = $value;
 }
 
 =head3 unset()
@@ -122,13 +155,18 @@ sub clear {
 
   $table->add($key, $value);
 
-Adds a new value to the table. If the value did not previously exist under the
-given key, it will be created. Otherwise, it will be added as a new value to
-the key.
+Adds a new value to the table. This method is the sole interface for adding
+mutiple values for a single key.
 
 =cut
 
 sub add {
+    # Issue a warning if the value is undefined.
+    if (! defined $_[2] and $^W) {
+        require Carp;
+        Carp::carp "Use of uninitialized value in null operation";
+        $_[2] = '';
+    }
     tied(%{shift()})->add(@_);
 }
 
@@ -136,18 +174,20 @@ sub add {
 
   $table->merge($key, $value);
 
-Merges a new value with an existing value by concatenating the new value onto
-the existing. The result is a comma-separated list of all of the values merged
-for a given key.
+Merges a new value with an existing value by appending the new value to the
+existing. The result is a string with the old value separated from the new by
+a comma and a space. If C<$key> contains multiple values, then only the first
+value will be used before appending the new value, and the remaining values
+will be discarded.
 
 =cut
 
 sub merge {
     my ($self, $key, $value) = @_;
-    if (defined $self->{$key}) {
-        $self->{$key} .= ',' . $value;
+    if (exists $self->{$key}) {
+        $self->{$key} .= ', ' . $value;
     } else {
-        $self->{$key} = "$value";
+        $self->{$key} = $value;
     }
 }
 
@@ -157,9 +197,9 @@ sub merge {
 
 Pass a code reference to this method to have it iterate over all of the
 key/value pairs in the table. Keys will multiple values will trigger the
-execution of the code reference multiple times for each value. The code
+execution of the code reference multiple times, once for each value. The code
 reference should expect two arguments: a key and a value. Iteration terminates
-when the code reference returns false, to be sure to have it return a true
+when the code reference returns false, so be sure to have it return a true
 value if you wan it to iterate over every value in the table.
 
 =cut
@@ -180,52 +220,53 @@ sub do {
 # object is based on.
 package Apache::FakeTableHash;
 use strict;
+my %curr_keys;
 
 sub TIEHASH {
     my $class = shift;
     return bless {}, ref $class || $class;
 }
 
+# Values are always stored as strings in an array.
 sub STORE {
     my ($self, $key, $value) = @_;
-    $self->{lc $key} = [ $key => ref $value ? "$value" : $value ];
+    # Issue a warning if the value is undefined.
+    if (! defined $value and $^W) {
+        require Carp;
+        Carp::carp "Use of uninitialized value in null operation";
+        $value = '';
+    }
+    $self->{lc $key} = [ $key => ["$value"] ];
 }
 
 sub add {
-    my ($self, $key) = (shift, shift);
-    return unless defined $_[0];
-    # Stringify value.
-    my $value = ref $_[0] ? "$_[0]" : $_[0];
+    my ($self, $key, $value) = @_;
     my $ckey = lc $key;
     if (exists $self->{$ckey}) {
-        # Add it to the array or create the array.
-        if (ref $self->{$ckey}[1]) {
-            push @{$self->{$ckey}[1]}, $value;
-        } else {
-            $self->{$ckey}[1] = [ $self->{$ckey}[1], $value ];
-        }
+        # Add it to the array,
+        push @{$self->{$ckey}[1]}, "$value";
     } else {
         # It's a simple assignment.
-        $self->{$ckey} = [ $key => $value ];
+        $self->{$ckey} = [ $key => ["$value"] ];
     }
 }
 
 sub DELETE {
     my ($self, $key) = @_;
     my $ret = delete $self->{lc $key};
-    return $ret->[1];
+    return $ret->[1][0];
 }
 
 sub FETCH {
-    my ($self, $key) = @_;
+    my $self = shift;
+    my $key = lc shift;
     # Grab the values first so that we don't autovivicate the key.
-    my $val = $self->{lc $key} or return;
-    if (my $ref = ref $val->[1]) {
-        return unless $val->[1][0];
-        # Return the first value only.
-        return $val->[1][0];
-    }
-    return $val->[1];
+    my $val = $self->{$key} or return;
+    # If the key is the current key, return the value that's next. Otherwise,
+    # return the first value.
+    return $curr_keys{$self} && $curr_keys{$self}->[0] eq $key
+      ? $val->[1][$curr_keys{$self}->[1]]
+      : $val->[1][0];
 }
 
 sub get {
@@ -233,7 +274,8 @@ sub get {
     my $ckey = lc $key;
     # Prevent autovivication.
     return unless exists $self->{$ckey};
-    return $self->{$ckey}[1] unless ref $self->{$ckey}[1];
+    # Return the array in an array context and just the first value in a
+    # scalar context.
     return wantarray ? @{$self->{$ckey}[1]} : $self->{$ckey}[1][0];
 }
 
@@ -246,23 +288,38 @@ sub EXISTS {
     return exists $self->{lc $key};
 }
 
+my $keyer = sub {
+    my $self = shift;
+    # Get the next key via perl's iterator.
+    my $key = each %$self;
+    # If there's no key, clear out our tracking of the current key and return.
+    delete $curr_keys{$self}, return unless defined $key;
+    # Cache the key and array index 0 for NEXTKEY and FETCH to use.
+    $curr_keys{$self} = [ $key => 0 ];
+    return $self->{$key}[0];
+};
+
 sub FIRSTKEY {
     my $self = shift;
-    # Reset perl's iterator.
+    # Reset perl's iterator and then get the key.
     keys %$self;
-    # Get the first key via perl's iterator.
-    my $first_key = each %$self;
-    return undef unless defined $first_key;
-    return $self->{$first_key}[0];
+    $self->$keyer;
 }
 
 sub NEXTKEY {
-    my ($self, $nextkey) = @_;
-    # Get the next key via perl's iterator.
-    my $next_key = each %$self;
-    return undef unless defined $next_key;
-    return $self->{$next_key}[0];
+    my ($self, $last_key) = @_;
+    # Return the last key if there are more values to be fetched for it.
+    my $ckey = lc $last_key;
+    return $last_key
+      if $curr_keys{$self}->[0] eq $ckey
+      && ++$curr_keys{$self}->[1] <= $#{$self->{$ckey}[1]};
+
+    # Otherwise, just get the next key.
+    $self->$keyer;
 }
+
+# Just be sure to clear out the current key.
+sub DESTROY { delete $curr_keys{shift()}; }
 
 1;
 __END__
